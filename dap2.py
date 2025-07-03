82538bd0478c382d0d9a170cd6023a4b9ba1c263
maddyk-anjali <2ke23mc023@kleit.ac.in>
2025-07-03 08:53:30 +0530

    front_end commit

--- dap2.py
+++ dap2.py
@@ -2,7 +2,8 @@ def insert_into_sorted_list(arr, element):
     arr.append(element)
     arr.sort()  # Ensuring the list remains sorted
     return arr
-#ffff
+#gggg
+#madhu
 # Taking input from the user
 arr = list(map(int, input("Enter the sorted list elements separated by space: ").split()))
 element = int(input("Enter the element to insert: "))
